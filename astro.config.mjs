cfdd5cf442190ba893a62f0d7384f1509a6ab8d7
William <williamxtez14@gmail.com>
2023-10-17 16:16:05 -0500

    [UPDATE] pre finalized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-// import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      height: theme => ({
-        "screen/1.5": "calc(100vh / 1.5)",
-        "screen/2": "50vh",
-        "screen/3": "calc(100vh / 3)",
-        "screen/4": "calc(100vh / 4)",
-        "screen/5": "calc(100vh / 5)",
-      }),
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			height: (theme) => ({
+				"screen/1.5": "calc(100vh / 1.5)",
+				"screen/2": "50vh",
+				"screen/3": "calc(100vh / 3)",
+				"screen/4": "calc(100vh / 4)",
+				"screen/5": "calc(100vh / 5)"
+			}),
+			colors: {
+				"green-waqanki": "#3fb54b"
+			}
+		}
+	},
+	plugins: []
+};
